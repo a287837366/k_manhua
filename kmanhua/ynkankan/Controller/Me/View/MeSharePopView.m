e7196d3d2ad53045585699b37b5464bd7a3ab8ab
fdong1 <fdong1@yahoo.com>
2016-02-01 09:55:56 +0800

    Revert "分享VIew"
    This reverts commit 0a6adf2bbb67d5143a4ffc85f925b26a6ec40773.

--- kmanhua/ynkankan/Controller/Me/View/MeSharePopView.m
+++ kmanhua/ynkankan/Controller/Me/View/MeSharePopView.m
@@ -9,16 +9,14 @@
 #import "MeSharePopView.h"
 #import "AppConstant.h"
 
-#define Ainmation_Time  0.4
-
 
 @interface MeSharePopView(){
 
-    UIButton *blockView;
+    UIView *blockView;
     UIWindow *mainWindow;
-    UIView *shareView;
     
 }
+
 
 
 @end
@@ -31,16 +29,11 @@
     
     if (self) {
         
-        blockView = [[UIButton alloc] init];
+        blockView = [[UIView alloc] init];
         blockView.frame = CGRectMake(0, 0, kScreenWidth, kScreenHeight);
-        self.frame = CGRectMake(0, 0, kScreenWidth, kScreenHeight);
         blockView.backgroundColor = [UIColor blackColor];
         blockView.alpha = 0;
-        
-        [blockView addTarget:self action:@selector(touchBlockView1:) forControlEvents:UIControlEventTouchUpInside];
-        
-        [self createShareView];
-        
+
     }
     
     return self;
@@ -48,83 +41,13 @@
 }
 
 -(void)show{
-    
-    mainWindow = [[UIWindow alloc] init];
-    mainWindow.backgroundColor = [UIColor clearColor];
-    mainWindow.frame = CGRectMake(0, 0, kScreenWidth, kScreenHeight);
-    
-    [mainWindow addSubview:blockView];
-    [mainWindow addSubview:shareView];
-    [mainWindow makeKeyAndVisible];
-    
-    [UIView animateWithDuration:Ainmation_Time
-                     animations:^{
-                         blockView.alpha = 0.4;
-                         shareView.frame = CGRectMake(0, kScreenHeight - 170, kScreenWidth, 170);
-                     }
-                     completion:^(BOOL finised){
+    UIView *view = [UIApplication sharedApplication].keyWindow;
 
-                     }
-     ];
+    blockView.alpha = 0.3;
+    [view addSubview:blockView];
+    
 }
 
 
-#pragma mark - 创建
--(void)createShareView{
-
-    shareView = [[UIView alloc] init];
-    shareView.frame = CGRectMake(0, kScreenHeight, kScreenWidth, 170);
-    shareView.backgroundColor = [UIColor whiteColor];
-    
-    
-    UIView *lineView = [[UIView alloc] init];
-    lineView.frame = CGRectMake(0, 40, kScreenWidth, 1);
-    lineView.backgroundColor = Color_eeeeee;
-    [shareView addSubview:lineView];
-    
-    UIButton *cancleButton = [[UIButton alloc] init];
-    cancleButton.frame = CGRectMake(kScreenWidth - 36, 7, 26, 26);
-    cancleButton.alpha = 0.5;
-    [cancleButton addTarget:self action:@selector(touchBlockView1:) forControlEvents:UIControlEventTouchUpInside];
-    [cancleButton setBackgroundImage:[UIImage imageNamed:@"me_weixin_cancle"] forState:UIControlStateNormal];
-    [shareView addSubview:cancleButton];
-    
-    UIButton *memontBtn = [[UIButton alloc] init];
-    memontBtn.frame = CGRectMake(20, 65, 60, 60);
-    [memontBtn setImage:[UIImage imageNamed:@"me_weixin_group"] forState:UIControlStateNormal];
-    [shareView addSubview:memontBtn];
-    
-    
-    UIButton *weixinFriendBtn = [[UIButton alloc] init];
-    weixinFriendBtn.frame = CGRectMake(100, 65, 60, 60);
-    [weixinFriendBtn setImage:[UIImage imageNamed:@"me_weixin_share"] forState:UIControlStateNormal];
-    [shareView addSubview:weixinFriendBtn];
-}
-
-#pragma mark - Action
-//点击阴影
--(void)touchBlockView1:(UIButton *)button{
-    NSLog(@"touchBlockView");
-    [UIView animateWithDuration:Ainmation_Time
-                     animations:^{
-                        blockView.alpha = 0;
-                        shareView.frame = CGRectMake(0, kScreenHeight, kScreenWidth, 260);
-                     }
-                     completion:^(BOOL finised){
-                         [blockView removeFromSuperview];
-                         blockView = nil;
-                         
-                         [shareView removeFromSuperview];
-                         shareView = nil;
-                         
-                         [mainWindow resignKeyWindow];
-                         mainWindow = nil;
-                         
-                        [self removeFromSuperview];
-                     }
-     ];
-
-    
-}
 
 @end
