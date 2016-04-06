fe7e5d7d8d7a10738fc3243ead2ff0d31087549e
fdong1 <fdong1@yahoo.com>
2016-04-06 17:20:15 +0800

    no message

--- kmanhua/ynkankan/Lib/AmPhotoPiker/AmPhotoGroupViewController.h
+++ kmanhua/ynkankan/Lib/AmPhotoPiker/AmPhotoGroupViewController.h
@@ -12,18 +12,9 @@
 
 @interface AmPhotoGroupViewController : UIViewController
 
-@property (nonatomic, assign) id<AmPhotoGroupViewDelegate> delegate;
-
 //可选择最大数量
 @property (nonatomic, assign) NSInteger maxCount;
 
 @property (nonatomic, weak) id<AmPhotoDidSelectedDelegate> selecteDelegate;
-
-@end
-
-
-@protocol AmPhotoGroupViewDelegate <NSObject>
-
--(void)goBack;
 
 @end
