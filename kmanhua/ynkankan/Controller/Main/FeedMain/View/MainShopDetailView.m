7192ad16d2f6601ac78332a7a4703f0e9f6cba11
fdong1 <fdong1@yahoo.com>
2016-06-15 11:45:15 +0800

    添加图片点击效果

--- kmanhua/ynkankan/Controller/Main/FeedMain/View/MainShopDetailView.m
+++ kmanhua/ynkankan/Controller/Main/FeedMain/View/MainShopDetailView.m
@@ -10,6 +10,8 @@
 #import "AppConstant.h"
 #import "UIImageView+WebCache.h"
 #import "ToolsClass.h"
+#import "MJPhoto.h"
+#import "MJPhotoBrowser.h"
 
 #define Item_Image_H    (kScreenWidth / 1.7f)
 
@@ -19,6 +21,7 @@
 @property (strong, nonatomic) UIScrollView *pictureScollView;
 @property (strong, nonatomic) UILabel *countLable;
 @property (strong, nonatomic) NSArray * arr;
+@property (strong, nonatomic) NSMutableArray *clickImageArray;
 
 @property (strong, nonatomic) UILabel *titleLable;
 @property (strong, nonatomic) UILabel *detailLable;
@@ -57,6 +60,8 @@
     self.pictureScollView.showsVerticalScrollIndicator = NO;
     self.pictureScollView.delegate = self;
     
+    self.clickImageArray = [[NSMutableArray alloc] init];
+    
     for (int i = 0; i < imageArray.count; i++) {
     
         UIImageView *imageView = [[UIImageView alloc] init];
@@ -64,7 +69,18 @@
         imageView.contentMode = UIViewContentModeScaleAspectFill;
         [imageView sd_setImageWithURL:[NSURL URLWithString:imageArray[i]]];
         
+        UIButton *allButton = [[UIButton alloc] init];
+        allButton.frame = imageView.frame;
+        allButton.tag = 100 + i;
+        [allButton addTarget:self action:@selector(clickDetailImage:) forControlEvents:UIControlEventTouchUpInside];
+        
+        MJPhoto *photo = [[MJPhoto alloc] init];
+        photo.url = [NSURL URLWithString:imageArray[i]];
+        [photo.srcImageView sd_setImageWithURL:[NSURL URLWithString:imageArray[i]]];
+        [self.clickImageArray addObject:photo];
+        
         [self.pictureScollView addSubview:imageView];
+        [self.pictureScollView addSubview:allButton];
     }
     
     self.pictureScollView.contentSize = CGSizeMake(imageArray.count * kScreenWidth, 0);
@@ -84,9 +100,18 @@
     [self addSubview:self.countLable];
 }
 
+- (void)clickDetailImage:(UIButton *)button{
+
+    MJPhotoBrowser *browser = [[MJPhotoBrowser alloc] init];
+    browser.currentPhotoIndex = button.tag - 100; // 弹出相册时显示的第一张图片是？
+    browser.photos = self.clickImageArray; // 设置所有的图片
+    [browser show];
+    
+}
+
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
 
-    NSLog(@">>>>> %d",  (int)(scrollView.contentOffset.x/ kScreenWidth));
+//    NSLog(@">>>>> %d",  (int)(scrollView.contentOffset.x/ kScreenWidth));
     self.countLable.text = [NSString stringWithFormat:@"%d/%lu", (int)(scrollView.contentOffset.x/ kScreenWidth) + 1, (unsigned long)self.arr.count];
 }
 
@@ -119,6 +144,7 @@
     self.titleLable.textColor = Color_666666;
     self.titleLable.font = [UIFont systemFontOfSize:15];
     self.titleLable.text = title;
+    self.titleLable.numberOfLines = 2;
     
     [self addSubview:self.titleLable];
     
